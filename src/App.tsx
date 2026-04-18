5af4a25e40d1342eca25043816ac496a7129d1dc
bornfly <bornflyborntochange@outlook.com>
2026-04-19 00:02:25 +0800

    auto-snapshot: before claude edit

--- src/App.tsx
+++ src/App.tsx
@@ -308,6 +308,7 @@ const AppContent: React.FC = () => {
               <Route path="/prv-demo" element={<PRVDemo />} />
               <Route path="/font-compare" element={<FontCompare />} />
               <Route path="/resources" element={<ResourcesConsolePage />} />
+              <Route path="/resources_claude" element={<UsageLimitsView />} />
               <Route path="/excalidraw" element={<div className="-m-4 md:-m-6 w-[calc(100%+2rem)] md:w-[calc(100%+3rem)] h-full"><ExcalidrawView /></div>} />
               <Route path="*" element={<Navigate to="/home" replace />} />
             </Routes>
